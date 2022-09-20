690d483fdcbb74b01dbd9bb89097d7616c7373f4
Narrow03 <richardowilliam03@gmail.com>
2022-09-20 15:16:11 +0700

    menghapus command

--- app/src/main/java/com/example/q_rier/MainMenu.kt
+++ app/src/main/java/com/example/q_rier/MainMenu.kt
@@ -8,7 +8,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainMenu : AppCompatActivity() {
 
     private lateinit var binding : BottomNavigationView
-//gguiig
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
